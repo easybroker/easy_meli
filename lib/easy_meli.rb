ac17f789f4688149d38a4fc1a8772969dd6c41f8
Eric Northam <eric@northam.us>
2020-05-18 18:19:12 -0500

    Added helpers in EasyMeli doc but decided to updated old instance methods names which broke interface. Bumped to 0.4.0

--- lib/easy_meli.rb
+++ lib/easy_meli.rb
@@ -16,4 +16,21 @@ module EasyMeli
   def self.configure
     yield(configuration)
   end
+
+  def self.authorization_url(country_code, redirect_uri)
+    EasyMeli::AuthorizationClient.authorization_url(country_code, redirect_uri)
+  end
+
+  def self.create_token(code, redirect_uri, logger: nil)
+    EasyMeli::AuthorizationClient.create_token(code, redirect_uri, logger: nil)
+  end
+
+  def self.refresh_token(refresh_token, logger: nil)
+    EasyMeli::AuthorizationClient.refresh_token(refresh_token, logger: nil)
+  end
+
+  def self.api_client(access_token: nil, refresh_token: nil, logger: nil)
+    access_token = self.refresh_token(refresh_token, logger: logger) if refresh_token
+    EasyMeli::ApiClient.new(access_token, logger: logger)
+  end
 end
--- test/easy_meli_test.rb
+++ test/easy_meli_test.rb
@@ -10,4 +10,34 @@ class EasyMeliTest < Minitest::Test
     assert_equal 'test_app_id', EasyMeli.configuration.application_id
     assert_equal 'test_secret_key', EasyMeli.configuration.secret_key
   end
+
+  def test_authorization_url
+    assert EasyMeli.authorization_url(:ar, 'test')
+  end
+
+  def test_create_token
+    EasyMeli::AuthorizationClient.expects(:create_token).with('foo', 'bar', logger: nil)
+    EasyMeli.create_token('foo', 'bar')
+  end
+
+  def test_refresh_token
+    EasyMeli::AuthorizationClient.expects(:refresh_token).with('foo', logger: nil)
+    EasyMeli.refresh_token('foo')
+  end
+
+  def test_api_client_with_refresh_token
+    EasyMeli::AuthorizationClient.expects(:refresh_token).with('foo', logger: nil).returns('bar')
+    EasyMeli::ApiClient.expects(:new).with('bar', logger: nil)
+    EasyMeli.api_client(refresh_token: 'foo')
+  end
+
+  def test_api_client_with_access_token
+    EasyMeli::ApiClient.expects(:new).with('bar', logger: nil)
+    EasyMeli.api_client(access_token: 'bar')
+  end
+
+  def test_api_client_without_tokens
+    EasyMeli::ApiClient.expects(:new).with(nil, logger: nil)
+    EasyMeli.api_client
+  end
 end
